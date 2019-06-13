903a644b58cd3bee5a742f1504db8ed9f0de8bc6
xmcc <946864395@qq.com>
2019-06-13 20:50:19 +0800

    asd

---
+++ src/main/java/com/example/git1/Git1Application.java
@@ -0,0 +1,16 @@
+package com.example.git1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git1Application.class, args
+
+        );
+        System.out.println("asd");
+    }
+
+}
